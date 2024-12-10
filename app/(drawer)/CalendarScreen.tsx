e1f93dbe1c933ad4ac59fd066e352a67d11b9c64
Sunscar-Sony <2201641530154@psit.ac.in>
2024-12-10 16:25:27 +0530

    Drawer Navigator Updated

---
+++ app/(drawer)/CalendarScreen.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function CalendarScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>This is Calendar Screen</Text>
+    </View>
+  );
+}
