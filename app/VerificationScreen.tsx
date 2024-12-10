475169905b30d2d41e00ff324101d5c5f8a9699b
Sunscar-Sony <2201641530154@psit.ac.in>
2024-12-10 16:07:04 +0530

    Stack Navigation Screens Updated

---
+++ app/VerificationScreen.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function VerificationScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>This is Verification Screen</Text>
+    </View>
+  );
+}
