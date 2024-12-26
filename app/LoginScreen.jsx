d4f4cc48487b99806c6a96c79d44f0b9dae64029
Sunscar-Sony <2201641530154@psit.ac.in>
2024-12-26 14:26:10 +0530

    LoginScreen Updated

--- app/LoginScreen.jsx
+++ app/LoginScreen.jsx
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   StyleSheet,
   Image,
+  FlatList,
 } from "react-native";
 import { useRouter } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
@@ -26,53 +27,71 @@ export default function LoginScreen() {
     router.push("/RegisterScreen");
   };
 
-  return (
-    <View style={styles.container}>
-      <Image
-        source={require("../assets/images/ignitia.png")}
-        style={styles.logo}
-      />
-      <View style={styles.inputWrapper}>
-        <Text style={styles.title}>Sign In</Text>
-        <View style={styles.inputContainer}>
-          <Ionicons name="call-outline" size={24} color="#fff" />
-          <TextInput
-            style={styles.input}
-            placeholder="+91"
-            placeholderTextColor="#ffffffaa"
-            keyboardType="phone-pad"
-          />
-        </View>
-        <View style={styles.inputContainer}>
-          <Ionicons name="lock-closed-outline" size={24} color="#fff" />
-          <TextInput
-            style={styles.input}
-            placeholder="Your Password"
-            placeholderTextColor="#ffffffaa"
-            secureTextEntry={true}
-          />
-        </View>
-        <TouchableOpacity onPress={handleForget}>
-          <Text style={styles.forgetPassword}>Forget Password</Text>
-        </TouchableOpacity>
-        <LinearGradient
-          colors={["#D19A08", "#FEDB7E", "#D19A08"]}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 0 }}
-          style={styles.signInButton}
-        >
-          <TouchableOpacity onPress={handleLogin}>
-            <Text style={styles.signInText}>SIGN IN</Text>
+  const data = [
+    {
+      id: 'inputWrapper',
+      component: (
+        <View style={styles.inputWrapper}>
+          <Text style={styles.title}>Sign In</Text>
+          <View style={styles.inputContainer}>
+            <Ionicons name="call-outline" size={24} color="#fff" />
+            <TextInput
+              style={styles.input}
+              placeholder="+91"
+              placeholderTextColor="#ffffffaa"
+              keyboardType="phone-pad"
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Ionicons name="lock-closed-outline" size={24} color="#fff" />
+            <TextInput
+              style={styles.input}
+              placeholder="Your Password"
+              placeholderTextColor="#ffffffaa"
+              secureTextEntry={true}
+            />
+          </View>
+          <TouchableOpacity onPress={handleForget}>
+            <Text style={styles.forgetPassword}>Forget Password</Text>
           </TouchableOpacity>
-        </LinearGradient>
-        <TouchableOpacity style={styles.signUpWrapper} onPress={handleSignUp}>
-          <Text style={styles.signUpText}>
-            Don't have an account?{" "}
-            <Text style={styles.signUpLink}>Sign Up</Text>
-          </Text>
-        </TouchableOpacity>
+          <LinearGradient
+            colors={["#D19A08", "#FEDB7E", "#D19A08"]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 0 }}
+            style={styles.signInButton}
+          >
+            <TouchableOpacity onPress={handleLogin}>
+              <Text style={styles.signInText}>SIGN IN</Text>
+            </TouchableOpacity>
+          </LinearGradient>
+          <TouchableOpacity style={styles.signUpWrapper} onPress={handleSignUp}>
+            <Text style={styles.signUpText}>
+              Don't have an account?{" "}
+              <Text style={styles.signUpLink}>Sign Up</Text>
+            </Text>
+          </TouchableOpacity>
+        </View>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <View style={styles.header}>
+        <Image
+          source={require("../assets/images/ignitia.png")}
+          style={styles.logo}
+        />
       </View>
-    </View>
+      <View style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => item.component}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={styles.flatListContainer}
+      />
+      </View>
+    </>
   );
 }
 
@@ -83,8 +102,18 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 20,
   },
+  flatListContainer: {
+    width: "100%",
+    alignItems: "center",
+  },
+  header: {
+    backgroundColor: "#0E0019",
+    width: "100%",
+    height: 180,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   logo: {
-    top: 30,
     width: 240,
     height: 90,
   },
@@ -96,10 +125,8 @@ const styles = StyleSheet.create({
     fontWeight: "600",
   },
   inputWrapper: {
-    marginTop: -30,
-    flex: 1,
     width: "100%",
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignSelf: "center",
   },
   inputContainer: {
@@ -136,7 +163,7 @@ const styles = StyleSheet.create({
   },
   signInButton: {
     alignSelf: "center",
-    width: "70%",
+    width: 220,
     height: 50,
     borderRadius: 14,
     alignItems: "center",
