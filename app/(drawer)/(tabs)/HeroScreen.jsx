e883539e9ece7e321af7458d03a8e24dc91f662f
Sunscar-Sony <2201641530154@psit.ac.in>
2024-12-29 19:01:32 +0530

    Bottom Navbar added

--- app/(drawer)/(tabs)/HeroScreen.jsx
+++ app/(drawer)/(tabs)/HeroScreen.jsx
@@ -1,17 +1,27 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import Header from "@/components/Header";
+import SecondType from "@/components/SecondType";
 
 export default function HeroScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        backgroundColor: "#0E0019",
-      }}
-    >
-      <Header></Header>
+    <View style={styles.container}>
+      <Header />
       <Text>This is Home Screen</Text>
+      <SecondType style={styles.secondType} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#0E0019",
+    justifyContent: "flex-start",
+  },
+  secondType: {
+    position: "absolute",
+    bottom: 100,
+    right: 100,   
+  },
+});
