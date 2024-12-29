61d3b06c1e71039548570b4b098df80799f83355
Sunscar-Sony <2201641530154@psit.ac.in>
2024-12-29 18:34:35 +0530

    Create HomeScreen.jsx

---
+++ app/(drawer)/HomeScreen.jsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View
+      style={{
+        backgroundColor: "#0E0019",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>This is Home Screen Drawer</Text>
+    </View>
+  );
+}
