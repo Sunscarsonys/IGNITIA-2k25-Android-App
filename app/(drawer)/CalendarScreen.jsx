f73964d5e1fe74b64193655ca60b747be9a3e78e
Sanskar Soni <149942456+Sunscarsony@users.noreply.github.com>
2025-05-14 17:39:13 +0530

    Update CalendarScreen.jsx

--- app/(drawer)/CalendarScreen.jsx
+++ app/(drawer)/CalendarScreen.jsx
@@ -1,1 +1,16 @@
+import { Text, View } from "react-native";
 
+export default function CalendarScreen() {
+  return (
+    <View
+      style={{
+        backgroundColor: "#0E0019",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>This is Calendar Screen</Text>
+    </View>
+  );
+}
